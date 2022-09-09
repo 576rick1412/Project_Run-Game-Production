af0ae56d0f74c7dd2e2b7c72161cd066970c6dda
576rick1412 <576rick1412@gmail.com>
2022-09-10 05:43:40 +0900

    한글 폰트 추가 + 로딩화면 텍스트 추가
    로딩화면의 텍스트를 추가하여, 현재 스테이지의 번호와 정보를 볼 수 있도록 만듦
    
    tmi : 현재 유니티 에셋번들 기능을 사용해볼지 고민하는중, 구현에 어려움이 있어 보이지만 보안관련해서 이점이 많을 것으로 보임
    
    모두 즐거운 추석 되세용 ( 하트하트 )

---
+++ Assets/CS/1. inGame/Loading_Manager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Loading_Manager : MonoBehaviour
+{
+    static string nextScene;
+    static string Stage_Name;
+    static string Stage_Description;
+    [SerializeField] Image ProgressBar;
+
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI Description;
+
+    public static void LoadScene(string SceneName, string ST_Name, string ST_Description)
+    {
+        nextScene = SceneName;
+        Stage_Name = ST_Name; 
+        Stage_Description = ST_Description;
+        SceneManager.LoadScene("LoadingScene");
+    }
+    void Start()
+    {
+        Name.text = Stage_Name;
+        Description.text = Stage_Description;
+        StartCoroutine(LoadSceneProcess());
+    }
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0f;
+        while (!op.isDone)
+        {
+            yield return null;
+
+            if (op.progress < 0.9f)
+            {
+                ProgressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                ProgressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
+                if (ProgressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+}
--- Assets/CS/3. etc/Proto_move_Scene.cs
+++ Assets/CS/3. etc/Proto_move_Scene.cs
@@ -9,7 +9,8 @@ public class Proto_move_Scene : MonoBehaviour
     {
         if(Input.GetKeyUp(KeyCode.Space))
         {
-            Loading_Manager.LoadScene("Scene_2");
+            Loading_Manager.LoadScene("Scene_2","1 - 1", 
+                "세계의 정의를 위해 뛰어다니는 어느 용병의 이야기이다.");
         }
     }
 }
