11a2801a5ff2f32cdbe7fbc6516ed689f555432b
576rick1412 <576rick1412@gmail.com>
2022-09-11 02:36:35 +0900

    BGM 에셋 추가 + 스테이지씬 - 로딩씬 텍스트의 기능 추가
    엑셀을 통해 텍스트UI를 수정할 수 있도록 변경
    
    Start_Scene : 게임시작화면 + ( 임시 스테이지 선택창 )
    Start_Scene은 기능확인을 위한 임시용도이며, 차후 게임시작화면으로 바꿀 것입니다.
    
    현재 인게임에 대한 기획이 진행중이므로, UI와 메인 화면 등에 집중할 예정

--- Assets/CS/3. etc/Proto_move_Scene.cs
+++ Assets/CS/3. etc/Proto_move_Scene.cs
@@ -1,16 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class Proto_move_Scene : MonoBehaviour
 {
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI Description;
+
+    [SerializeField] int  branch;
+    [SerializeField] RunGame_EX RunGame_EX;
+
+    [SerializeField] public string Stage_Num;
+    [SerializeField] public string Stage_Des;
+
+    public GameObject[] BGI;
+    void Start()
+    {
+        Random_BGI();
+        STG_Excel();
+    }
+
     void Update()
     {
         if(Input.GetKeyUp(KeyCode.Space))
         {
-            Loading_Manager.LoadScene("Scene_2","1 - 1", 
-                "세계의 정의를 위해 뛰어다니는 어느 용병의 이야기이다.");
+            Loading_Manager.LoadScene("Scene_2", Stage_Num, Stage_Des);
+        }
+    }
+
+    void Random_BGI()
+    {
+        int num = Random.Range(0, BGI.Length);
+        for (int i = 0; i < BGI.Length; i++)
+        {
+            if (i == num) continue;
+            BGI[i].SetActive(false);
+        }
+    }
+
+    void STG_Excel()
+    {
+        for(int i = 0; i < RunGame_EX.STSheet.Count; ++i)
+        {
+            if (RunGame_EX.STSheet[i].ST_branch == branch)
+            {
+                Stage_Num = Name.text = RunGame_EX.STSheet[i].ST_name;
+                Stage_Des = Description.text = RunGame_EX.STSheet[i].ST_description;
+            }
         }
     }
 }
---
+++ Assets/Excel/RunGame_EX_Entity.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class RunGame_EX_Entity
+{
+    public int      ST_branch;
+    public string   ST_name;
+    public string   ST_description;
+}
