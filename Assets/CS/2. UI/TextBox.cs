c4ef2bbd8b8a0a9bd860065044f75f79ad056ab6
576rick1412 <576rick1412@gmail.com>
2022-09-11 09:13:55 +0900

    텍스트 수정 스크립트 분리 + 리소스 추가
    기존 시작화면의 랜덤 배경이미지를 보여주는 스크립트와
    텍스트를 수정하는 스크립트가 하나에 묶여 있어
    이를 분리해 텍스트만 따로 사용 가능하도록 수정함
    
    이후 씬을 분리할 것이기에 스크립트 또한 분리할 필요가 있었음.

---
+++ Assets/CS/2. UI/TextBox.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TextBox : MonoBehaviour
+{
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI Description;
+
+    [SerializeField] int branch;
+    [SerializeField] RunGame_EX RunGame_EX;
+
+    [SerializeField] public string Stage_Num;
+    [SerializeField] public string Stage_Des;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        STG_Excel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            Loading_Manager.LoadScene("Scene_2", Stage_Num, Stage_Des);
+        }
+    }
+    void STG_Excel()
+    {
+        for (int i = 0; i < RunGame_EX.STSheet.Count; ++i)
+        {
+            if (RunGame_EX.STSheet[i].ST_branch == branch)
+            {
+                Stage_Num = Name.text = RunGame_EX.STSheet[i].ST_name;
+                Stage_Des = Description.text = RunGame_EX.STSheet[i].ST_description;
+            }
+        }
+    }
+}
--- Assets/CS/3. etc/Proto_move_Scene.cs
+++ Assets/CS/3. etc/Proto_move_Scene.cs
@@ -1,33 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
 
 public class Proto_move_Scene : MonoBehaviour
 {
-    public TextMeshProUGUI Name;
-    public TextMeshProUGUI Description;
-
-    [SerializeField] int  branch;
-    [SerializeField] RunGame_EX RunGame_EX;
-
-    [SerializeField] public string Stage_Num;
-    [SerializeField] public string Stage_Des;
-
     public GameObject[] BGI;
     void Start()
     {
         Random_BGI();
-        STG_Excel();
     }
 
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            Loading_Manager.LoadScene("Scene_2", Stage_Num, Stage_Des);
-        }
+
     }
 
     void Random_BGI()
@@ -37,18 +23,6 @@ public class Proto_move_Scene : MonoBehaviour
         {
             if (i == num) continue;
             BGI[i].SetActive(false);
-        }
-    }
-
-    void STG_Excel()
-    {
-        for(int i = 0; i < RunGame_EX.STSheet.Count; ++i)
-        {
-            if (RunGame_EX.STSheet[i].ST_branch == branch)
-            {
-                Stage_Num = Name.text = RunGame_EX.STSheet[i].ST_name;
-                Stage_Des = Description.text = RunGame_EX.STSheet[i].ST_description;
-            }
         }
     }
 }
