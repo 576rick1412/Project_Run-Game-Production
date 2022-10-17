84a967a6ca3a4cd111511caf6b32b5beabfa2b2e
576rick1412 <576rick1412@gmail.com>
2022-10-17 13:05:15 +0900

    세팅버튼 수정
    버튼 클릭 후 창이 나왔을 때, 다른 곳을 클릭하면 창이 들어가도록 수정
    ( 기존 버튼을 누르면 창이 들어갔다 다시 나왔으나, 이 문제를 그냥 들어가도록 해서 고침 )

--- Assets/CS/3. Anime/StageButton_Anim.cs
+++ Assets/CS/3. Anime/StageButton_Anim.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class StageButton_Anim : MonoBehaviour
 {
-    bool OnStageCheck;
+    [SerializeField]bool OnStageCheck;
+    [SerializeField] bool OnCheck;
 
-    private Camera _MainCam;
     Animator anime;
 
-    private void Start() { anime = GetComponent<Animator>(); OnStageCheck = false; _MainCam = Camera.main; }
+    private void Start() { anime = GetComponent<Animator>(); OnStageCheck = false; OnCheck = false; }
 
-    private void Update()
-    {
-        // if (OnStageCheck == true && Input.anyKey) OnStageUI();
-    }
+    private void Update() { if (OnStageCheck == true && Input.anyKeyDown) { OnStageUI(); OnCheck = true; Invoke("OffCheck", 0.15f); } } 
 
     public void OnStageUI()  
     {
+        if (OnCheck == true) { OnCheck = false; return; }
+
         switch (OnStageCheck)
         {
             case true: anime.SetInteger("ButtonNum", 1); OnStageCheck = false; break;
             case false: anime.SetInteger("ButtonNum", 2); OnStageCheck = true; break;
         }
     }
+    void OffCheck() { OnCheck = false; }
 }
