4e6d8fca8c800bbb7b86af47d8edab030ae6b3da
576rick1412 <576rick1412@gmail.com>
2022-10-27 09:50:20 +0900

    인벤토리 아이템 수에 따른 길이가 변경되지 않던 버그 수정
    발표 전 긴급 패치

--- Assets/CS/1. inGame/Inventory/Inven_Instant.cs
+++ Assets/CS/1. inGame/Inventory/Inven_Instant.cs
@@ -10,14 +10,14 @@ public class Inven_Instant : MonoBehaviour
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
 
-        if (InventoryDB.IV.아이템.Length > 24 )
+        if (InventoryDB.IV.아이템.Length > 12 )
         {
             // 24를 빼면 UpSize가 0이 되길래 21을 빼서 계산하도록 수정함
             int UpSize = Mathf.CeilToInt((InventoryDB.IV.아이템.Length - 12) / 3);
 
             RectTransform rectTran = gameObject.GetComponent<RectTransform>();
 
-            int newwidth = ((Screen.width - 1000) + (260 * UpSize + 400));
+            int newwidth = ((Screen.width - 1000) + (280 * UpSize + 400));
 
             rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newwidth);
             rectTransform.position = new Vector2((newwidth) , (Screen.height / 2) - 50);
