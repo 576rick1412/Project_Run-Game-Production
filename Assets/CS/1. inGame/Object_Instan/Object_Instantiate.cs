268f848d58da113fe322d4c7859bf4cae5626a72
576rick1412 <576rick1412@gmail.com>
2023-01-25 02:03:58 +0900

    Revert "맵 생성 로직 추가"
    This reverts commit 70433eca060f5b185eacec91e6510413bf925ccd.

--- Assets/CS/1. inGame/Object_Instan/Object_Instantiate.cs
+++ Assets/CS/1. inGame/Object_Instan/Object_Instantiate.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class Object_Instantiate : MonoBehaviour
 {
-    [System.Serializable]
-    public struct Map
-    {
-        public GameObject[] spanwMaps; // 생성할 맵 목록
-    }
-    public Map[] maps;
+    [SerializeField] GameObject[] spanwMaps; // 생성할 맵 목록
+    int spanwIndex = 0; // 생성할 맵의 번호 - 시작은 0으로 고정
 
     void Awake()
     {
@@ -23,23 +19,17 @@ public class Object_Instantiate : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.V)) InstanceMap();
     }
 
     public void InstanceMap()
-    {
-        int mapIndex = 0; // 생성할 맵의 번호 - 시작은 0으로 고정
-        int spanwIndex = 0; // 생성할 맵의 세부 번호 - 시작은 0으로 고정
-
+    { 
         GameManager.GM.data.floorSpeedValue *= 1.05f;
         GameManager.GM.data.BGSpeedValue    *= 1.05f;
 
-        // 생성할 맵 지정 랜덤 돌리기
-        mapIndex = Random.Range(0, maps.Length);
-        spanwIndex = Random.Range(0, maps[mapIndex].spanwMaps.Length);
-
         Debug.Log("맵 생성");
-        Instantiate(maps[mapIndex].spanwMaps[spanwIndex],new Vector3(28, 0,0),Quaternion.identity);
+        Instantiate(spanwMaps[spanwIndex],new Vector3(28, 0,0),Quaternion.identity);
 
+        spanwIndex = Random.Range(0, spanwMaps.Length);
     }
 }
